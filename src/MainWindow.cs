4358d0f8f4ca26f36f758ab12b4e7a386d717cdc
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-06-19 18:46:49 +0530

    Cleanup.

--- src/MainWindow.cs
+++ src/MainWindow.cs
@@ -58,7 +58,7 @@ class MainWindow : Window
             Child = new System.Windows.Forms.WebBrowser
             {
                 ScrollBarsEnabled = false,
-                DocumentText = $@"<head><meta http-equiv=""X-UA-Compatible"" content=""IE=9""/></head><body style=""background-color: #1E1E1E""><div style=""width: 100%; height: 100%; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%)"">{(global::Resources.Minecraft)}</div></body>"
+                DocumentText = $@"<head><meta http-equiv=""X-UA-Compatible"" content=""IE=9""/></head><body style=""background-color:#1E1E1E""><div style=""width:100%;height:100%;position:absolute;left:50%;top:50%;transform:translate(-50%, -50%)"">{(global::Resources.Minecraft)}</div></body>"
             },
             IsEnabled = false
         };
