0f42242b21d8f3b8b8ef9955d2cfe7ee03e21f91
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-10-09 09:22:43 +0530

    Cleanup.

--- src/Program.cs
+++ src/Program.cs
@@ -13,7 +13,6 @@ static class Program
     static void Main(string[] args)
     {
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
-        using Mutex mutex = new(true, "C7B58EAD-356C-40A1-A145-7262C3C04D00", out bool createdNew); if (!createdNew) return;
         AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
         {
             var exception = (Exception)e.ExceptionObject;
@@ -21,6 +20,7 @@ static class Program
             ShellMessageBox(lpcText: exception.Message);
             Environment.Exit(0);
         };
+        using Mutex mutex = new(true, "C7B58EAD-356C-40A1-A145-7262C3C04D00", out bool createdNew); if (!createdNew) return;
         new MainWindow(args.FirstOrDefault()?.Equals("/preview", StringComparison.OrdinalIgnoreCase) ?? false).ShowDialog();
     }
 }
