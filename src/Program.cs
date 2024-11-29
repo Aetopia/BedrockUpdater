a456e35532c9612bc47c8ce05e6556d896389fc6
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-11-29 16:36:38 +0530

    Format.

--- src/Program.cs
+++ src/Program.cs
@@ -13,6 +13,5 @@ static class Program
         using Mutex mutex = new(true, "C7B58EAD-356C-40A1-A145-7262C3C04D00", out bool createdNew);
         if (!createdNew || RuntimeInformation.OSArchitecture is not Architecture.X64) return;
         new Window(_.Any(_ => _.Equals("/preview", StringComparison.OrdinalIgnoreCase))).ShowDialog();
-        
     }
 }
