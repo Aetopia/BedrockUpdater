6aed515dce950392034bd4621d6d35d0ea607088
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-08-22 14:42:59 +0530

    Format.

--- src/Product.cs
+++ src/Product.cs
@@ -1,18 +1,19 @@
 sealed class Product
 {
-    internal readonly string ProductId;
-
-    internal readonly string PackageFamilyName;
-
     internal Product(string productId, string packageFamilyName)
     {
         ProductId = productId;
         PackageFamilyName = packageFamilyName;
     }
 
-    internal static readonly Product Xbox = new("9WZDNCRD1HKW", "Microsoft.XboxIdentityProvider_8wekyb3d8bbwe");
+    static Product()
+    {
+        Release = new("9NBLGGH2JHXJ", "Microsoft.MinecraftUWP_8wekyb3d8bbwe");
+        Xbox = new("9WZDNCRD1HKW", "Microsoft.XboxIdentityProvider_8wekyb3d8bbwe");
+        Preview = new("9P5X4QVLC2XR", "Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe");
+    }
 
-    internal static readonly Product Release = new("9NBLGGH2JHXJ", "Microsoft.MinecraftUWP_8wekyb3d8bbwe");
+    internal readonly string ProductId, PackageFamilyName;
 
-    internal static readonly Product Preview = new("9P5X4QVLC2XR", "Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe");
+    internal static readonly Product Xbox, Release, Preview;
 }
